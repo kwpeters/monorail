1c31c435db6bcb596fbae57fa1788f13c02ee285
Kevin Peters <kwpeters@gmail.com>
2023-04-20 16:40:27 -0400

    Added prompts from depot.

---
+++ packages/depot-node/src/prompts.ts
@@ -0,0 +1,138 @@
+import * as _ from "lodash-es";
+import inquirer from "inquirer";
+
+
+/**
+ * Prompts the user to confirm whether they want to continue.
+ * @param message - The message to display
+ * @param defaultToConfirm - true to make confirmation the default response.
+ * false to make canceling the default.
+ * @return A promise that is resolved with a boolean indicating whether the user
+ * has confirmed (true) or not (false).
+ */
+export async function promptToContinue(
+    message:          string,
+    defaultToConfirm: boolean
+): Promise<boolean> {
+    const questionConfirmation = {
+        type:    "confirm",
+        name:    "confirm",
+        default: defaultToConfirm,
+        message: message || "Continue?"
+    };
+
+    const answers = await inquirer.prompt<{ confirm: boolean; }>([questionConfirmation]);
+    return answers.confirm;
+}
+
+
+/**
+ * Prompts the user to enter a string.
+ * @param message - The prompt message to display
+ * @param defaultValue - The default input value
+ * @return A promise that resolves with the string the user entered.
+ */
+export async function promptForString(
+    message: string,
+    defaultValue?: string
+): Promise<string> {
+    const question = {
+        type:    "input",
+        name:    "inputValue",
+        message: message,
+        default: defaultValue
+    };
+
+    const answers = await inquirer.prompt<{ inputValue: string; }>([question]);
+    return answers.inputValue;
+}
+
+
+/**
+ * Interface defining the properties of a choice of type string.
+ */
+export interface IChoiceString {
+    name: string;
+    value: string;
+}
+
+
+/**
+ * Prompts the user to choose one of the specified choices.
+ * @param message - The prompt to display
+ * @param choices - The choices to present to the user.
+ * @return A promise that resolves with the chosen choice's value.
+ */
+export async function promptForChoice(
+    message: string,
+    choices: Array<IChoiceString>
+): Promise<string> {
+    const actualChoices =
+        choices.map((curChoice) => ({name: curChoice.name, value: curChoice.value, short: curChoice.name}));
+
+    const question = {
+        type:    "list",
+        name:    "inputValue",
+        message: message,
+        choices: actualChoices
+    };
+
+    const answers = await inquirer.prompt<{inputValue: string}>([question]);
+    return answers.inputValue;
+}
+
+
+/**
+ * Prompts the user to choose one of the provided choices or to enter their own
+ * "other" string value.
+ * @param message - The prompt to display
+ * @param choices - The provided choices to present along with "other"
+ * @return A promise that resolves with the entered string. If the user selected
+ * from the choices, the choice's value is returned.  If the user chose to enter
+ * another value, that value is returned.
+ */
+export async function promptForStringWithChoices(
+    message: string,
+    choices: Array<IChoiceString>
+): Promise<string> {
+    const otherValue = "otherValueXyzzy";
+    const actualChoices =
+        _.chain(choices)
+        .map((curChoice) => ({name: curChoice.name, value: curChoice.value, short: curChoice.name}))
+        .concat({name: "other", value: otherValue, short: "other"})
+        .value();
+
+    const question = {
+        type:    "list",
+        name:    "inputValue",
+        message: message,
+        choices: actualChoices
+    };
+
+    const answers = await inquirer.prompt<{inputValue: string}>([question]);
+    return answers.inputValue === otherValue ?
+        promptForString(message) :
+        answers.inputValue;
+}
+
+
+/**
+ * Prompts the user to enter a string via their default editor.
+ * @param message - The prompt message to display
+ * @param defaultValue - The default input value
+ * @return A promise that resolves with the string the user entered.
+ */
+export async function promptForStringInEditor(
+    message: string,
+    defaultValue?: string
+): Promise<string> {
+    const question = {
+        type:    "editor",
+        name:    "editorInput",
+        message: message,
+        default: defaultValue
+    };
+
+    const answers = await inquirer.prompt<{ editorInput: string; }>([question]);
+    return answers.editorInput;
+}
