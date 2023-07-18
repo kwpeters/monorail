0366e268cb0bb4ccba6be2e25d4a05e4b3134a06
Kevin Peters <kwpeters@gmail.com>
2023-07-18 15:45:41 -0400

    Added unit test demonstrating how to check Jasmine version.

---
+++ packages/depot-node/test/jasmineVersion.spec.ts
@@ -0,0 +1,20 @@
+function getJasmineVersion(): string {
+    const versionStr: string =
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-call
+        (jasmine as any).version as string || (jasmine.getEnv() as any).versionString() as string;
+    return versionStr;
+}
+
+
+beforeAll(() => {
+    console.log("Running Jasmine " + getJasmineVersion() + ".");
+});
+
+
+describe("jasmine", () => {
+
+    it("version will be printed while running the unit tests", () => {
+        expect(getJasmineVersion()).toBe("3.10.1");
+    });
+
+});
