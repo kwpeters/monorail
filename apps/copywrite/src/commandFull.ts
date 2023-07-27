26e1ec64440ed41816795f17c117aefe06625c3e
Kevin Peters <kwpeters@gmail.com>
2023-07-27 10:26:47 -0400

    Added --allownonexistent option to "copywrite full".

--- apps/copywrite/src/commandFull.ts
+++ apps/copywrite/src/commandFull.ts
@@ -15,8 +15,25 @@ export function builder(argv: Argv): Argv {
         describe: "The destination directory",
         type:     "string"
     })
+    .option(
+        "allownonexistent",
+        {
+            demandOption: false,
+            type:         "boolean",
+            default:      false,
+            describe:     "allows sourceDir and destDir to not exist and returns with a successful exit code"
+        }
+    )
     .check(
         (argv: Arguments) => {
+
+            if (argv.allownonexistent) {
+                // Don't bother checking to see if sourceDir and destDir exist.
+                return true;
+            }
+
+            // Make sure sourceDir and destDir exist.  If either don't exist
+            // exit with a non-zero exit code.
             const sourceDir = new Directory(argv.sourceDir as PathPart);
             const destDir = new Directory(argv.destDir as PathPart);
 
@@ -38,10 +55,17 @@ export function builder(argv: Argv): Argv {
 export async function handler(args: Arguments): Promise<void> {
 
     const sourceDir = new Directory(args.sourceDir as PathPart);
+
+    // If the source directory does not exist, there is nothing to do.
+    if (!sourceDir.existsSync()) {
+        return;
+    }
+
     const destDir = new Directory(args.destDir as PathPart);
 
     await destDir.empty()
     .then(() => {
+        // destDir will be created if non-existent
         return sourceDir.copy(destDir, false);
     });
 }
