29aa57752dcf16ebc6767d3e9347f06832d5fe66
Kevin Peters <kwpeters@ra.rockwell.com>
2023-10-27 15:57:50 -0400

    Added identity().

---
+++ packages/depot/src/functional.ts
@@ -0,0 +1,9 @@
+/**
+ * The identity function.
+ *
+ * @param arg - The item that will be returned.
+ * @return The value that was passed in
+ */
+export function identity<T>(arg: T): T {
+    return arg;
+}
