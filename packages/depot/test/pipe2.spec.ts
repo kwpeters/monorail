8a8bf356165bd75f6c67e02eed13ee81239e6ee5
Kevin Peters <kwpeters@ra.rockwell.com>
2023-10-30 15:02:41 -0400

    Resurected function version of pipe().

---
+++ packages/depot/test/pipe2.spec.ts
@@ -0,0 +1,20 @@
+import { pipe } from "../src/pipe2.js";
+
+describe("pipe()", () => {
+
+
+    it("pipes a value through the specified functions", () => {
+        const result =
+            pipe(
+                "5",
+                (str) => parseInt(str, 10),
+                (n) => n * 3,
+                (n) => n + 1,
+                (n) => n.toString(),
+                (str) => str + "!"
+            );
+        expect(result).toEqual("16!");
+    });
+
+
+});
