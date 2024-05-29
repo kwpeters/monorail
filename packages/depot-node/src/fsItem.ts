520a7538a2c7b0522992a34364103f07f4ee9c2b
Kevin Peters <kwpeters@ra.rockwell.com>
2024-05-29 09:34:58 -0400

    Added deleteFsItem().

--- packages/depot-node/src/fsItem.ts
+++ packages/depot-node/src/fsItem.ts
@@ -1,6 +1,9 @@
 import * as fs from "fs";
 import * as fsp from "fs/promises";
 import { Result, FailedResult, SucceededResult } from "../../depot/src/result.js";
+import { PromiseResult } from "../../depot/src/promiseResult.js";
+import { assertNever } from "../../depot/src/never.js";
+import { pipeAsync } from "../../depot/src/pipeAsync2.js";
 import { errorToString } from "../../depot/src/errorHelpers.js";
 import { Directory } from "./directory.js";
 import { File } from "./file.js";
@@ -32,4 +35,34 @@ export async function fsPathToFsItem(path: FsPath): Promise<Result<FsItem, strin
     else {
         return new FailedResult(`Filesystem item "${path.toString()}" has unknown type.`);
     }
+}
+
+
+
+export async function deleteFsItem(fsItem: FsItem): Promise<Result<FsItem, string>> {
+    if (fsItem instanceof Directory) {
+        return fsItem.delete()
+        .then(
+            () => new SucceededResult(fsItem),
+            (err) => new FailedResult(errorToString(err))
+        );
+    }
+    else if (fsItem instanceof File) {
+        return fsItem.delete()
+        .then(
+            () => new SucceededResult(fsItem),
+            (err) => new FailedResult(errorToString(err))
+        );
+    }
+    else if (fsItem instanceof Symlink) {
+        return pipeAsync(
+            fsItem.delete(),
+            (res) => PromiseResult.mapSuccess(() => fsItem, res)
+        );
+    }
+    else {
+        assertNever(fsItem);
+    }
+
+
 }
