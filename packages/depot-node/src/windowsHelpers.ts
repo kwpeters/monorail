ad55b474c3ea9e07c57c5bb99c140b24d981aa5e
Kevin Peters <kwpeters@ra.rockwell.com>
2024-06-01 14:36:22 -0400

    Added getUncPath().

--- packages/depot-node/src/windowsHelpers.ts
+++ packages/depot-node/src/windowsHelpers.ts
@@ -1,6 +1,8 @@
+import { FailedResult, Result, SucceededResult } from "../../depot/src/result.js";
 import { spawn } from "./spawn2.js";
 import { File } from "./file.js";
 import { Directory } from "./directory.js";
+import { FsItem } from "./fsItem.js";
 
 
 export async function launchAdmin(executable: File, cwd: Directory | undefined = undefined): Promise<boolean> {
@@ -11,9 +13,38 @@ export async function launchAdmin(executable: File, cwd: Directory | undefined =
         `Start-Process -Verb RunAs "${executable.toString()}"`
     ];
 
-    // const options = cwd ? {cwd: cwd.toString()} : undefined;
     const options = cwd ? { cwd: "c:\\" } : undefined;
 
     const spawnRes = await spawn(cmd, args, options).closePromise;
     return spawnRes.succeeded;
+}
+
+
+export async function getUncPath(fsItem: FsItem): Promise<Result<string, string>> {
+    const abspath = fsItem.absPath();
+
+    const driveLetterRegex = /^(?<driveLetter>[a-z]):/i;
+    const driveMatch = driveLetterRegex.exec(abspath);
+    if (!driveMatch) {
+        return new FailedResult(`Path "${abspath}" does not start with a drive letter.`);
+    }
+
+    const driveLetter = driveMatch.groups!.driveLetter;
+    const spawnRes = await spawn("net", ["use", `${driveLetter}:`], { cwd: "c:\\" }).closePromise;
+
+    if (spawnRes.failed) {
+        return new FailedResult(`The path "${abspath}" is not a network path.`);
+    }
+
+    const remoteNameRegex = /^.*Remote name\s+\\\\(?<server>\w+)\\(?<share>\w+)$/im;
+    const remoteMatch = remoteNameRegex.exec(spawnRes.value);
+    if (!remoteMatch) {
+        return new FailedResult(`Could not find server and share in "net use" output.`);
+    }
+
+    const server = remoteMatch.groups!.server;
+    const share = remoteMatch.groups!.share;
+
+    const unc = `\\\\${server}\\${share}${abspath.slice(2)}`;
+    return new SucceededResult(unc);
 }
---
+++ packages/depot-node/test/windowsHelpers.spec.ts
@@ -0,0 +1,12 @@
+import {Directory} from "../../depot-node/src/directory.js";
+import { getUncPath } from "../src/windowsHelpers.js";
+describe("getUncPath()", () => {
+
+    // it("replaces the drive letter with the server and share names", async () => {
+    //     const dir = new Directory("H:\\dev\\rok\\lemans\\LogixVM");
+    //     const uncRes = await getUncPath(dir);
+    //     expect(uncRes.succeeded).toBeTrue();
+    //     expect(uncRes.value!).toContain("floyd");
+    // });
+
+});
