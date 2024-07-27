1b2d697374f9b88e265cda6da71284eaa61758e5
Kevin Peters <kwpeters@ra.rockwell.com>
2024-07-27 17:55:05 -0400

    Created importPhotos app.

---
+++ apps/importPhotos/src/importPhotos.ts
@@ -0,0 +1,4 @@
+import { SucceededResult } from "../../../packages/depot/src/result.js";
+
+const res = new SucceededResult("Success");
+console.log(`Result: ${res.toString()}`);
---
+++ apps/importPhotos/test/sample-test.spec.ts
@@ -0,0 +1,7 @@
+describe("importPhotos", () => {
+
+    it("should pass", () => {
+        expect(true).toBeTruthy();
+    });
+
+});
