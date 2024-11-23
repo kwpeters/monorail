2deab9d545e497e241aac5787a6e9e9f12316b84
Kevin Peters <kwpeters@gmail.com>
2024-11-23 16:21:12 -0500

    Switched app from ava unit tests to jasmine.

--- apps/crypto-cli/src/app.test.tsx
+++ apps/crypto-cli/src/app.test.tsx
@@ -1,21 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/naming-convention
 import React from "react";
 import chalk from "chalk";
-import test from "ava";
-import {render} from "ink-testing-library";
+import { render } from "ink-testing-library";
 // eslint-disable-next-line @typescript-eslint/naming-convention
 import App from "./app.js";
 
 
-test("greet unknown user", (t) => {
-    const {lastFrame} = render(<App name={undefined} />);
 
-    t.is(lastFrame(), `Hello, ${chalk.green("Stranger")}`);
+it("greet unknown user", () => {
+    const { lastFrame } = render(<App name={ undefined } />);
+    expect(lastFrame()).toBe(`Hello, ${chalk.green("Stranger")}`);
 });
 
 
-test("greet user with a name", (t) => {
-    const {lastFrame} = render(<App name="Jane" />);
-
-    t.is(lastFrame(), `Hello, ${chalk.green("Jane")}`);
+it("greet user with a name", () => {
+    const { lastFrame } = render(<App name="Jane" />);
+    expect(lastFrame()).toBe(`Hello, ${chalk.green("Jane")}`);
 });
